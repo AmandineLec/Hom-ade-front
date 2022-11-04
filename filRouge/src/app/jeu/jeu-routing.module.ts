5a43ed1b6ce25efd56ecd58afe2dc80bb1e12d4c
YannickCho <yannick.choquenet@gmail.com>
2022-11-04 16:49:50 +0100

    ajout background map-centre

--- filRouge/src/app/jeu/jeu-routing.module.ts
+++ filRouge/src/app/jeu/jeu-routing.module.ts
@@ -28,7 +28,9 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class JeuRoutingModule { }

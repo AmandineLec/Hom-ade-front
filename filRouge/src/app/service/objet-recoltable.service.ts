a2e2f9348b057822b78fae0a83bbf18b24e4e4eb
YannickCho <yannick.choquenet@gmail.com>
2022-11-07 16:52:29 +0100

    ajout getObjetsRecoltables

--- filRouge/src/app/service/objet-recoltable.service.ts
+++ filRouge/src/app/service/objet-recoltable.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ObjetRecoltable } from '../interface/objet-recoltable';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,10 @@ export class ObjetRecoltableService {
   private objetRecoltableUrl = '';
 
   constructor(private http: HttpClient) { }
+
+  getObjetsRecoltables(): Observable<ObjetRecoltable[]> {
+    return this.http.get<ObjetRecoltable[]>(this.objetRecoltableUrl);
+  }
 
   
 }

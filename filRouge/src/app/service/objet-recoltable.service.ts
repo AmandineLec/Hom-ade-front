4c3333e0146efd14f561e62420df89d731474274
YannickCho <yannick.choquenet@gmail.com>
2022-11-07 17:05:07 +0100

    ajout getObjetRecoltable(id)

--- filRouge/src/app/service/objet-recoltable.service.ts
+++ filRouge/src/app/service/objet-recoltable.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ObjetRecoltable } from '../interface/objet-recoltable';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,25 @@ export class ObjetRecoltableService {
   constructor(private http: HttpClient) { }
 
   getObjetsRecoltables(): Observable<ObjetRecoltable[]> {
-    return this.http.get<ObjetRecoltable[]>(this.objetRecoltableUrl);
+    return this.http.get<ObjetRecoltable[]>(this.objetRecoltableUrl)
+    .pipe(
+      catchError(this.handleError<ObjetRecoltable[]>('getObjetsRecoltables', []))
+    );
   }
 
+  getObjetRecoltable(id: number): Observable<ObjetRecoltable> {
+    const url = '${this.objetRecoltableUrl}/${id}';
+    return this.http.get<ObjetRecoltable>(url).pipe(
+      catchError(this.handleError<ObjetRecoltable>('getObjetRecoltable id=${id}'))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      console.log('${operation} failed: ${error.message}');
+      return of(result as T);
+    }
+  }
   
 }

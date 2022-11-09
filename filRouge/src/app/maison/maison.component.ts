5fd3e0f51301bff7976743e4e7f0319635bdd97b
Marie Cartier <mariecartier50@gmail.com>
2022-11-09 17:06:56 +0100

    murs + sols + cadre ok, autres objets a terminer

--- filRouge/src/app/maison/maison.component.ts
+++ filRouge/src/app/maison/maison.component.ts
@@ -8,24 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaisonComponent implements OnInit {
   cadre: string = "../../assets/img/Divers/ObjetAPlacer.png";
-  cadre2: string = "../../assets/img/Divers/ObjetAPlacer.png";
-  sol: string = "../../assets/img/Divers/ObjetAPlacer.png";
+  table: string = "../../assets/img/Divers/ObjetAPlacer.png";
+  etagere: string = "../../assets/img/Divers/ObjetAPlacer.png"
+  chaise: string = "../../assets/img/Divers/ObjetAPlacer.png"
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  display(e:Event){
+  installeCadre(){
     this.cadre = "../../assets/img/Décoration/Cadre01.png";
     document.getElementById('cadre')!.style.gridRow = "4 / span 2";
   }
-  display2(e:Event){
-    this.cadre2 = "../../assets/img/Décoration/Cadre01.png";
-    document.getElementById('cadre')!.style.gridRow = "3 / span 2";
+
+  installeEtagere(){
+    // this.etagere = "../../assets/img/Meuble/Etagere/Etagere01.png";
+    // document.getElementById('etagere')!.style.gridRow = "9 / span 2";
+    // document.getElementById('imgEtagere')!.style.width = "150%";
+    // document.getElementById('imgEtagere')!.style.rotate = "0 0 0 0deg";
+
   }
-  deco(e:Event){
-    document.getElementById
+
+  deco(){
+  this.table = "../../assets/img/Meuble/Table/Table01.png";
+  document.getElementById('table')!.style.gridColumn = "6 / span 2";
+  document.getElementById('table')!.style.gridRow = "8 / span 2";
+  }
+
+  fond(){
+    let fond = document.getElementById('fond')!;
+    fond.style.backgroundColor = "transparent";
+    fond.style.backgroundImage = "url('../../assets/img/Décoration/Carrelage01.png')";
   }
 
 
+
+  gauche(){
+    let gauche = document.getElementById('chaise')!;
+    gauche.style.borderColor = "transparent";
+    gauche.style.borderLeft = "1px solid #EEAB00";
+    gauche.style.backgroundImage = "url('../../assets/img/Décoration/Carrelage01.png')";
+  }
+
+  bas(){
+    let bas = document.getElementById('bas')!;
+    bas.style.borderColor = "transparent";
+    bas.style.borderTop = "1px solid #EEAB00";
+    bas.style.backgroundImage = "url('../../assets/img/Décoration/parquet.jpg')";
+  }
 }

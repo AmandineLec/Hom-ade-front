5b26648977db818478f6a973da9a9e362a3b97c1
YannickCho <yannick.choquenet@gmail.com>
2022-11-10 09:31:50 +0100

    ajout recolte dans map-nord-est

--- filRouge/src/app/map-nord-est/map-nord-est.component.ts
+++ filRouge/src/app/map-nord-est/map-nord-est.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ObjetRecoltableService } from '../service/objet-recoltable.service';
+import { ObjetRecoltable } from '../interface/objet-recoltable';
 @Component({
   selector: 'app-map-nord-est',
   templateUrl: './map-nord-est.component.html',
   styleUrls: ['./map-nord-est.component.scss']
 })
 export class MapNordEstComponent implements OnInit {
+  objetsRecoltables: ObjetRecoltable[] = [];
 
-  constructor() { }
+  constructor(private objRecSer: ObjetRecoltableService) { }
 
   ngOnInit(): void {
+    this.objRecSer.getObjetsRecoltables().subscribe((objetsRecoltables) => {
+      this.objetsRecoltables = objetsRecoltables;
+    });
+  }
+
+  recolte(index: number) {
+    this.objRecSer.recolte(index).subscribe((objetRecoltable) => {
+      this.objetsRecoltables[index] = objetRecoltable;
+    });
   }
 
 }

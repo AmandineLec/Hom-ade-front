e962ac91958af3a0759ca754f6389fb23e22ece0
YannickCho <yannick.choquenet@gmail.com>
2022-11-10 10:13:09 +0100

    ajout recolte dans map-sud-est

--- filRouge/src/app/map-sud-est/map-sud-est.component.ts
+++ filRouge/src/app/map-sud-est/map-sud-est.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ObjetRecoltableService } from '../service/objet-recoltable.service';
+import { ObjetRecoltable } from '../interface/objet-recoltable';
 @Component({
   selector: 'app-map-sud-est',
   templateUrl: './map-sud-est.component.html',
   styleUrls: ['./map-sud-est.component.scss']
 })
 export class MapSudEstComponent implements OnInit {
+  objetsRecoltables: ObjetRecoltable[] = [];
 
-  constructor() { }
-
+  constructor(private objRecSer: ObjetRecoltableService) { }
+    
   ngOnInit(): void {
+    this.objRecSer.getObjetsRecoltables().subscribe((objetsRecoltables) => {
+      this.objetsRecoltables = objetsRecoltables;
+    });
   }
 
+  recolte(index: number) {
+    this.objRecSer.recolte(index).subscribe((objetRecoltable) => {
+      this.objetsRecoltables[index] = objetRecoltable;
+    });
+  }
 }
